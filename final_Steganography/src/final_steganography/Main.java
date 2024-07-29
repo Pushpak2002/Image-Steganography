9a41f95b38bfc8c3f58c6ab7987c081a68f59018
Pushpak2002 <91221694+Pushpak2002@users.noreply.github.com>
2024-07-29 11:00:24 +0530

    Add files via upload

---
+++ final_Steganography/src/final_steganography/Main.java
@@ -0,0 +1,21 @@
+package final_steganography;
+
+/*to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Pushpak
+ */
+public class Main {
+    
+           public static void main(String[] args){
+               
+               MainMenu mm = new MainMenu();
+               mm.setVisible(true);
+               mm.setLocationRelativeTo(null);
+               
+    }
+}
